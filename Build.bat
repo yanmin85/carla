6b98d30ea3e9e4027f4941e837a3f8c0f3109de7
nsubiron <nsubiron@cvc.uab.es>
2019-03-07 16:36:35 +0100

    Stuff

--- Build.bat
+++ Build.bat
@@ -1,8 +1,47 @@
 @echo off
 setlocal
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+if NOT DEFINED VCINSTALLDIR (
+    if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" (
+        call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+        echo "USING VISUAL STUDIO 17"
+    )
+)
 
-cl.exe
+if NOT DEFINED VCINSTALLDIR (
+    if exist "C:\Program Files (x86)\Microsoft Visual Studio 15.0\VC\vcvarsall.bat" (
+        call "C:\Program Files (x86)\Microsoft Visual Studio 15.0\VC\vcvarsall.bat" amd64
+        echo "USING VISUAL STUDIO 15"
+    )
+)
+
+if NOT DEFINED VCINSTALLDIR (
+    if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
+        call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+        echo "USING VISUAL STUDIO 14"
+    )
+)
+
+if NOT DEFINED VCINSTALLDIR (
+    if exist "C:\Program Files (x86)\Microsoft Visual Studio 13.0\VC\vcvarsall.bat" (
+        call "C:\Program Files (x86)\Microsoft Visual Studio 13.0\VC\vcvarsall.bat" amd64
+        echo "USING VISUAL STUDIO 13"
+    )
+)
+
+if NOT DEFINED VCINSTALLDIR (
+    if exist "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" (
+        call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" amd64
+        echo "USING VISUAL STUDIO 12"
+    )
+)
+
+echo "Using " %VCINSTALLDIR%
+
+if NOT DEFINED VCINSTALLDIR (
+    echo "No compatible visual studio found! run vcvarsall.bat first!"
+)
+
+make setup
 
 endlocal
